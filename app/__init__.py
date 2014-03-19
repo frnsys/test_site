d116f77beb7305f78a44520e82ca283ed008d860
Francis Tseng <ftzeng@gmail.com>
2014-03-19 18:13:43 -0400

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+app = Flask(__name__, static_folder='static', static_url_path='')
+app.config.from_object('config')
+
+from app import routes
---
+++ ping.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while [ 1 ]
+do
+	curl 192.168.1.144:5000
+  sleep 2
+done
